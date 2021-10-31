2449065d5a5c34b5f610ae7345ab394e46a700bf
Yulia <semina.yulia@bk.ru>
2021-10-31 19:31:09 +0300

    UserServiceImpl modify

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -13,34 +13,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
+    UserDao userDao = new UserDaoJDBCImpl();
 
     public void createUsersTable() {
-        UserDao userDao = new UserDaoJDBCImpl();
         userDao.createUsersTable();
     }
 
     public void dropUsersTable() {
-        UserDao userDao = new UserDaoJDBCImpl();
         userDao.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        UserDao userDao = new UserDaoJDBCImpl();
         userDao.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-        UserDao userDao = new UserDaoJDBCImpl();
         userDao.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        UserDao userDao = new UserDaoJDBCImpl();
         return userDao.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        UserDao userDao = new UserDaoJDBCImpl();
         userDao.cleanUsersTable();
     }
 }
